76362dce556793b74f0abc11625c306f48bb0b02
Amol Goel <amol.goel@traderinteractive.com>
2021-06-12 21:33:27 +0530

    first submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMAtrix will create a list of getters and setters of matrix and inverse functions
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NA
+  setMatrix<-function(y){ 
+  x<<-y
+  i<<-NA
+}
+getMatrix <- function() x
+setInverse <-function(i) i<<-solve
+getInverse <-function(x) i
+  list(set=setMatrix,get=getMatrix,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve will first check if inverse is available in present. x is a named list 
 cacheSolve <- function(x, ...) {
+  if (!x$getInverse()=NA){
+    return (x$getInverse())
+  }
+  else{
+    matrix <- x$getMatrix()
+    i<-solve(matrix)
+    setInverse(i)
+    return (i)
+    }
+  
+  
         ## Return a matrix that is the inverse of 'x'
 }
